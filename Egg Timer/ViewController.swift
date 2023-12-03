1ee50841751e960edad820f0748031a644deb0aa
Kitty <77138477+kamork@users.noreply.github.com>
2023-12-02 22:08:28 -0800

    Improved layout

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.titleLabel)
+    }
+    
 
 }
 
